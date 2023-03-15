84748c6e3a8d5228d2f21dfb9294455da25df8ed
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-03-14 23:07:32 -0300

    feat: add new handler

--- kong/app/main.go
+++ kong/app/main.go
@@ -6,7 +6,12 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!\n"))
 }
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	http.Template(w, "home.html", nil)
+}
+
 func main() {
 	http.HandleFunc("/", LoginHandler)
+	http.HandleFunc("/home", HomeHandler)
 	http.ListenAndServe(":8080", nil)
 }
